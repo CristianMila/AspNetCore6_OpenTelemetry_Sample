b32f140b3e7801104d4f70aa6b97a4130fb67582
Cristian Milá <cristian.mila92@gmail.com>
2022-09-09 21:37:56 +0200

    Test for adding tag on current activity from a service.

--- SomeApplicationProject/RocketService.cs
+++ SomeApplicationProject/RocketService.cs
@@ -1,9 +1,13 @@
-﻿namespace SomeApplicationProject
+﻿using System.Diagnostics;
+
+namespace SomeApplicationProject
 {
     public class RocketService
     {
         public string LaunchRocketById(int rocketId)
         {
+            Activity.Current?.AddTag("tag from appService", "yes");
+
             return $"Liftoff! Rocket with ID: {rocketId}";
         }
     }
